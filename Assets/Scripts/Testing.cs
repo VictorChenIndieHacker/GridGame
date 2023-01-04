254896b68b149891c9845d97654563bff8f56808
VictorChenIndieHacker <victorchen2045@outlook.com>
2023-01-04 21:25:01 +0800

    GridSystem Initial Commit

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+public class Testing : MonoBehaviour
+{
+    private Grid grid;
+
+    private void Start()
+    {
+        grid = new Grid(4, 2,2f,new Vector3(2,0));
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(),56);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+    }
+}
