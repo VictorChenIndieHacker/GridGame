e4def31a442918b7b254a5631ea1f08ff631d153
VictorChenIndieHacker <victorchen2045@outlook.com>
2023-01-20 21:09:35 +0800

    Camera system corefactoring

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class CameraSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] bool useEdgeScrolling=false;
 
     // Update is called once per frame
     void Update()
+    {
+        HandleCameraMovement();
+        if (useEdgeScrolling)
+        {
+            HandleCameraMovementEdgeScrolling();
+        }
+        HandleCameraRotation();
+    }
+
+    private void HandleCameraMovement()
     {
         Vector3 inputDir = Vector3.zero;
 
@@ -20,15 +26,42 @@ public class CameraSystem : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) inputDir.x = -1f;
         if (Input.GetKey(KeyCode.D)) inputDir.x = +1f;
 
+
+        
+
+
         Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
         float moveSpeed = 50f;
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        transform.position += moveSpeed * Time.deltaTime * moveDir;
 
+    }
+
+
+    private void HandleCameraMovementEdgeScrolling()
+    {
+        Vector3 inputDir = Vector3.zero;
+
+        
+        int edgeScrollSize = 20;
+        if (Input.mousePosition.x < edgeScrollSize) inputDir.x = -1f;
+        if (Input.mousePosition.y < edgeScrollSize) inputDir.z = -1f;
+        if (Input.mousePosition.x > Screen.width - edgeScrollSize) inputDir.x = +1f;
+        if (Input.mousePosition.y > Screen.height - edgeScrollSize) inputDir.z = +1f;
+
+        
+
+        Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
+        float moveSpeed = 50f;
+        transform.position += moveSpeed * Time.deltaTime * moveDir;
+    }
+
+    private void HandleCameraRotation()
+    {
         float rotateDir = 0f;
         if (Input.GetKey(KeyCode.Q)) rotateDir = +1f;
         if (Input.GetKey(KeyCode.E)) rotateDir = -1f;
 
         float rotateSpeed = 100f;
-        transform.eulerAngles += new Vector3(0, rotateDir*rotateSpeed*Time.deltaTime, 0);
+        transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);
     }
 }
